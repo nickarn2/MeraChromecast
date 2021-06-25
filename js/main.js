f0a923f66304f24c3d0039e7f03eb2a2c2bedb73
nickarn2 <nick_arn@yahoo.com>
2021-06-25 15:10:57 -0400

    test console log

--- js/main.js
+++ js/main.js
@@ -490,10 +490,14 @@ window.onload = function() {
     const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
     castDebugLogger.setEnabled(true);
     castDebugLogger.info(1, "My Message");
-    console.log(Constants.APP_INFO, 'My Message 2');
+    console.log('My Message 2');
     castDebugLogger.info("nn", "My Message 3");
 
     castDebugLogger.info(2, "My Message 4");
+
+    castDebugLogger.info(Constants.APP_INFO, 'My Message 5');
+
+
     FastCast.init(Constants.APP_NAMESPACE, function(){
         FastCast.onSenderConnected(function(event) {
             console.log(Constants.APP_INFO, 'Received Sender Connected event: ' + event.data);
