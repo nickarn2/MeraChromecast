ef0941644bf99f78f894580e3525503eef55f83b
nickarn2 <nick_arn@yahoo.com>
2021-07-01 10:29:53 -0400

    func copyToClipboard()

--- js/main.js
+++ js/main.js
@@ -489,6 +489,7 @@ window.onload = function() {
 //nn clipboard test    
 debugger;
 var one = window.location.href;
+copyToClipboard(one);
 //onsole.log("Clipboard-test", Received Sender Connected event: ' + event.data
 console.log("Clipboard-test", 'Received Sender Connected event: ' + one);
 var promise = navigator.clipboard.writeText(one);
@@ -531,3 +532,28 @@ tvApp.init();
         FastCast.connect();
     });
 };
+
+function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+        return window.clipboardData.setData("Text", text);
+
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        }
+        finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
