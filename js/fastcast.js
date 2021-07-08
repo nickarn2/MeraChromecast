281ff931d6e0584e39261a2fc8cc895273f46dd5
nickarn2 <nick_arn@yahoo.com>
2021-07-08 12:46:18 -0400

    do not send

--- js/fastcast.js
+++ js/fastcast.js
@@ -60,7 +60,7 @@ var FastCast = (function(){
 
     function sendTheMessage(data) {
         debugger;
-        context.sendCustomMessage("urn:x-cast:sncr-cloud", data);
+        //context.sendCustomMessage("urn:x-cast:sncr-cloud", data);
     }
     /**
      * Initializes FastCast. Expects 2 arguments: namespace
